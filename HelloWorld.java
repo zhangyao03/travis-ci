9d61b5b3a113e6a31bd7220b9ccfc7fc9be0d9ed
zhangyao03 <279045911@qq.com>
2016-08-31 09:50:29 +0800

    Delete HelloWorld.java

--- HelloWorld.java
+++
@@ -1,9 +0,0 @@
-public class HelloWorld {
-    /**
-    * 输出一行字符串“Hello World!”
-    * @param args
-    */
-    public static void main(String[] args) {
-    System.out.println("Hello World!");
-    }
-}
